dfcda3ef1b6a4f3a7b18526e8064ba707592c027
z1shivam <shivamk.services@gmail.com>
2023-08-12 05:45:08 +0530

    Version 2.0 Parivartan: Beta(Massive Update)

---
+++ sitewideJavascripts/toggleButton.js
@@ -0,0 +1,1 @@
+const toggleButton=document.getElementById("toggleButton");const contentDiv=document.getElementById("content");toggleButton.addEventListener("change",function(){if(this.checked){contentDiv.style.display="block"}else{contentDiv.style.display="none"}})
