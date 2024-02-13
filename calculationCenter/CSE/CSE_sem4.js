b105d46f3cd4930389dc9aa2896d82a28e1a6fc3
Shivam <118415844+z1shivam@users.noreply.github.com>
2024-02-13 09:58:51 +0000

    created calculator for sem 4th CSE

--- calculationCenter/CSE/CSE_sem4.js
+++ calculationCenter/CSE/CSE_sem4.js
@@ -1,15 +1,18 @@
-
-
 function calculateCGPA() {
     const getCredits = (marks) => marks > 100 ? 0 : marks > 90 ? 10 : marks > 80 ? 9 : marks > 70 ? 8 : marks > 60 ? 7 : marks > 50 ? 6 : marks > 40 ? 5 : marks > 30 ? 4 : marks > 20 ? 3 : marks > 10 ? 2 : marks > 0 ? 1 : 0;
 
     const getValue = (name) => parseFloat(document.getElementsByName(name)[0].value);
 
     const subjects = [
-        { c: 3, m: "Subject1_m" }, { c: 3, m: "Subject2_m" }, { c: 1, m: "Subject3_m" },
-        { c: 3, m: "Subject4_m" }, { c: 1, m: "Subject5_m" }, { c: 2, m: "Subject6_m" },
-        { c: 2, m: "Subject7_m" }, { c: 3, m: "Subject8_m" }, { c: 3, m: "Subject9_m" },
-        { c: 1, m: "Subject10_m" }, { c: 1, m: "Subject11_m" }
+        { c: 3, m: "OE1_m" },
+        { c: 3, m: "CSL3071_m" },
+        { c: 1, m: "CSP3071_m" },
+        { c: 3, m: "CSL3032_m" },
+        { c: 1, m: "CSP3032_m" },
+        { c: 3, m: "CSL3081_m" },
+        { c: 1, m: "CSP3081_m" },
+        { c: 3, m: "ECL2060_m" },
+        { c: 1, m: "ECP2060_m" }
     ];
 
     let totalPoints = 0;
@@ -22,18 +25,26 @@ function calculateCGPA() {
 
     const sgpaPointer = (totalPoints / totalCredits).toFixed(2);
     const toggleButton = document.getElementById("toggleButton");
+
     if (toggleButton.checked) {
+
         const inputElement = document.getElementById('cgpaInput');
         const inputValue = inputElement.value.trim();
+
         if (inputValue === '') {
             document.getElementById("displaySGPA").textContent = `SGPA: ${sgpaPointer}`;
-        } else {
-            let previousCGPA = parseFloat(document.getElementById('cgpaInput').value);
-            let newCGPA = ((previousCGPA + Number(sgpaPointer)) / 2).toFixed(2);
-            document.getElementById("displaySGPA").textContent = `SGPA: ${sgpaPointer} - CGPA: ${newCGPA} (approx)`;
         }
-    } else document.getElementById("displaySGPA").textContent = `SGPA: ${sgpaPointer}`;
+        else {
+            let previousCGPA = parseFloat(document.getElementById('cgpaInput').value);
+            let newCGPA = ((previousCGPA * 63 + Number(sgpaPointer)*19) / 82).toFixed(2);
+
+            document.getElementById("displaySGPA").textContent = `SGPA: ${sgpaPointer} - CGPA: ${newCGPA}`;
+
+        }
+    }
+    else document.getElementById("displaySGPA").textContent = `SGPA: ${sgpaPointer}`;
 }
+
 
 function resetForm() {
     const contentDiv = document.getElementById("content");
